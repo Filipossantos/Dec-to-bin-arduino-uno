9095efc4a4c9a527b714b7df726b7eca1cff0724
Filipossantos <85622683+Filipossantos@users.noreply.github.com>
2021-07-27 15:23:56 +0200

    Add files via upload

---
+++ GccApplication1/dectobin.h
@@ -0,0 +1,24 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 2021-07-27 14:58:53
+ *  Author: domis_000
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+void dectobin(int decnumber)
+{
+	DDRB |= (decnumber<<0);
+	while (1)
+	{
+		
+		PORTB |= (decnumber<<0);
+		
+	}
+}
+
+
+#endif /* INCFILE1_H_ */
---
+++ GccApplication1/main.c
@@ -0,0 +1,17 @@
+
+/*
+ * GccApplication1.c
+ *
+ * Created: 2021-07-26 10:54:13
+ * Author : domis_000
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include "dectobin.h"
+int main(void)
+{
+	
+dectobin(1);
+	
+}
